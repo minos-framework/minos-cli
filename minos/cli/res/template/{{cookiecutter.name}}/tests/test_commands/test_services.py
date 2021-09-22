33ece86ce6fe2faa5b4440df371ee4e8d6a797f1
Sergio García Prado <sergio.garcia@clariteia.com>
2021-09-22 17:16:12 +0200

    ISSUE #7 #8 * Add tests for `CommandService.create`. * Improve `isort` configuration.

--- minos/cli/res/template/{{cookiecutter.name}}/tests/test_commands/test_services.py
+++ minos/cli/res/template/{{cookiecutter.name}}/tests/test_commands/test_services.py
@@ -1,11 +1,16 @@
 import sys
 import unittest
 
-from src import (
-    {{ cookiecutter.aggregate }}CommandService,
+from minos.networks import (
+    Response,
 )
 
+from src import (
+    {{ cookiecutter.aggregate }},
+    {{ cookiecutter.aggregate }}CommandService,
+)
 from tests.utils import (
+    _FakeRequest,
     build_dependency_injector,
 )
 
@@ -23,8 +28,26 @@ class Test{{cookiecutter.aggregate}}CommandService(unittest.IsolatedAsyncioTestC
         await self.injector.unwire()
 
     def test_constructor(self):
-        obj = {{ cookiecutter.aggregate }}CommandService()
-        self.assertIsInstance(obj, {{cookiecutter.aggregate}}CommandService)
+        service = {{ cookiecutter.aggregate }}CommandService()
+        self.assertIsInstance(service, {{cookiecutter.aggregate}}CommandService)
+
+    async def test_create_{{ cookiecutter.aggregate.lower() }}(self):
+        service = {{ cookiecutter.aggregate }}CommandService()
+
+        request = _FakeRequest({})
+        response = await service.create_{{ cookiecutter.aggregate.lower() }}(request)
+
+        self.assertIsInstance(response, Response)
+
+        observed = await response.content()
+        expected = {{ cookiecutter.aggregate }}(
+            created_at=observed.created_at,
+            updated_at=observed.updated_at,
+            uuid=observed.uuid,
+            version=observed.version,
+        )
+
+        self.assertEqual(expected, observed)
 
 
 if __name__ == '__main__':
--- minos/cli/res/template/{{cookiecutter.name}}/tests/test_queries/test_services.py
+++ minos/cli/res/template/{{cookiecutter.name}}/tests/test_queries/test_services.py
@@ -4,7 +4,6 @@ import unittest
 from src import (
     {{ cookiecutter.aggregate }}QueryService,
 )
-
 from tests.utils import (
     build_dependency_injector,
 )
@@ -23,8 +22,8 @@ class Test{{cookiecutter.aggregate}}QueryService(unittest.IsolatedAsyncioTestCas
         await self.injector.unwire()
 
     def test_constructor(self):
-        obj = {{ cookiecutter.aggregate }}QueryService()
-        self.assertIsInstance(obj, {{cookiecutter.aggregate}}QueryService)
+        service = {{ cookiecutter.aggregate }}QueryService()
+        self.assertIsInstance(service, {{cookiecutter.aggregate}}QueryService)
 
 
 if __name__ == '__main__':
