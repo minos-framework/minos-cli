296ef9542bd3fcd94a0cc9d476cf7874a559d273
Sergio García Prado <sergio.garcia@clariteia.com>
2021-11-23 12:33:39 +0100

    ISSUE #17 * Add `rich` library.

--- minos/cli/__init__.py
+++ minos/cli/__init__.py
@@ -2,6 +2,9 @@ __author__ = """Clariteia Devs"""
 __email__ = "devs@clariteia.com"
 __version__ = "0.0.1"
 
+from .console import (
+    console,
+)
 from .constants import (
     MICROSERVICE_TEMPLATE_PATH,
 )
