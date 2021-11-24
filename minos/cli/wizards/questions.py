003e0f9814d21d7c9df03e02ce8e83b6c452b3de
Sergio García Prado <sergio.garcia@clariteia.com>
2021-11-24 12:52:36 +0100

    ISSUE #28 #29 * Add `secret` and `choices` support.

--- minos/cli/wizards/questions.py
+++ minos/cli/wizards/questions.py
@@ -34,13 +34,14 @@ class Question:
     """TODO"""
 
     def __init__(
-        self, name: str, type_: str, help_: Optional[str], choices: Optional, default: Optional,
+        self, name: str, type_: str, help_: Optional[str], choices: Optional, default: Optional, secret: bool = False
     ):
         self.name = name
         self.type_ = type_
         self.help_ = help_
         self.choices = choices
         self.default = default
+        self.secret = secret
 
     @classmethod
     def from_raw(cls, raw: dict[str, Any]) -> Question:
@@ -51,6 +52,7 @@ class Question:
             help_=raw.get("help", None),
             choices=raw.get("choices", None),
             default=raw.get("default", None),
+            secret=raw.get("secret", False),
         )
 
     def ask(self, context: dict[str, Any] = None, env: Optional[SandboxedEnvironment] = None) -> str:
@@ -70,9 +72,9 @@ class Question:
 
         return self._ask(title, default)
 
-    def _ask(self, title, default):
+    def _ask(self, title: str, default: Any) -> Any:
         answer = self._ask_fn(title, default=default)
-        while not self.valid(answer):
+        while not self.is_valid(answer):
             error_console.print(f"{answer!r} is invalid!")
             answer = self._ask_fn(title, default=default)
         return answer
@@ -87,7 +89,7 @@ class Question:
             fn = Confirm.ask
         else:
             fn = Prompt.ask
-        return partial(fn, console=console)
+        return partial(fn, console=console, choices=self.choices, password=self.secret)
 
     @property
     def title(self) -> str:
@@ -96,6 +98,6 @@ class Question:
             return self.help_
         return self.name
 
-    def valid(self, value: Any) -> bool:
+    def is_valid(self, value: Any) -> bool:
         """TODO"""
         return True
