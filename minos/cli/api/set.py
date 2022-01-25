3aa8dfbb648251d7194c231e34ed18bb27daa3b2
Alberto Amigo Alonso <albamigh@gmail.com>
2022-01-25 09:04:09 +0100

    - Refactored duplicated code.
    issue #46

--- minos/cli/api/set.py
+++ minos/cli/api/set.py
@@ -9,8 +9,8 @@ import yaml
 from ..consoles import (
     console,
 )
-from ..templating import TemplateFetcher
 from ..templating import (
+    TemplateFetcher,
     TemplateProcessor,
 )
 
@@ -20,112 +20,31 @@ app = typer.Typer(add_completion=False)
 
 
 @app.command("database")
-def database(
-    backend: str = typer.Argument(...),
-) -> None:
+def database(backend: str = typer.Argument(...),) -> None:
     """Set database configuration"""
-    path = Path(os.getcwd()) / ".minos-project.yaml"
-
-    if not path.exists():
-        console.print(Path.cwd().name)
-        console.print("No Minos project found. Consider 'minos project init'")
-        raise typer.Exit()
-
-    with path.open() as project_file:
-        data = yaml.load(project_file, Loader=yaml.FullLoader)
-        if "services" in data and not data["services"]:
-            data["services"] = dict()
-
-    if "services" in data and "database" in data["services"]:
-        console.print("Database already set")
-        raise typer.Exit()
-    else:
-        console.print(":wrench: Setting database config\n")
-        fetcher = TemplateFetcher.from_name(f"project-database-{backend}-init", VERSION)
-        processor = TemplateProcessor.from_fetcher(
-            fetcher,
-            Path.cwd(),
-            defaults={"project_name": Path.cwd().name}
-        )
-        processor.render()
-
-        data["database"] = backend
-        yaml.dump(data, sort_keys=False)
+    set_service("database", backend)
 
 
 @app.command("discovery")
-def discovery(
-    backend: str = typer.Argument(...),
-) -> None:
+def discovery(backend: str = typer.Argument(...),) -> None:
     """Set discovery configuration"""
-    path = Path(os.getcwd()) / ".minos-project.yaml"
-
-    if not path.exists():
-        console.print(Path.cwd().name)
-        console.print("No Minos project found. Consider 'minos project init'")
-        raise typer.Exit()
-
-    with path.open() as project_file:
-        data = yaml.load(project_file, Loader=yaml.FullLoader)
-        if "services" in data and not data["services"]:
-            data["services"] = dict()
-
-    if "services" in data and "discovery" in data["services"]:
-        console.print("Database already set")
-        raise typer.Exit()
-    else:
-        console.print(":wrench: Setting discovery config\n")
-        fetcher = TemplateFetcher.from_name(f"project-discovery-{backend}-init", VERSION)
-        processor = TemplateProcessor.from_fetcher(
-            fetcher,
-            Path.cwd(),
-            defaults={"project_name": Path.cwd().name}
-        )
-        processor.render()
-
-        data["discovery"] = backend
-        yaml.dump(data, sort_keys=False)
+    set_service("discovery", backend)
 
 
 @app.command("broker")
-def broker(
-    backend: str = typer.Argument(...),
-) -> None:
+def broker(backend: str = typer.Argument(...),) -> None:
     """Set broker configuration"""
-    path = Path(os.getcwd()) / ".minos-project.yaml"
-
-    if not path.exists():
-        console.print(Path.cwd().name)
-        console.print("No Minos project found. Consider 'minos project init'")
-        raise typer.Exit()
-
-    with path.open() as project_file:
-        data = yaml.load(project_file, Loader=yaml.FullLoader)
-        if "services" in data and not data["services"]:
-            data["services"] = dict()
-
-    if "services" in data and "broker" in data["services"]:
-        console.print("Database already set")
-        raise typer.Exit()
-    else:
-        console.print(":wrench: Setting broker config\n")
-        fetcher = TemplateFetcher.from_name(f"project-broker-{backend}-init", VERSION)
-        processor = TemplateProcessor.from_fetcher(
-            fetcher,
-            Path.cwd(),
-            defaults={"project_name": Path.cwd().name}
-        )
-        processor.render()
-
-        data["broker"] = backend
-        yaml.dump(data, sort_keys=False)
+    set_service("broker", backend)
 
 
 @app.command("api-gateway")
-def apigateway(
-    backend: str = typer.Argument(...),
-) -> None:
+def apigateway(backend: str = typer.Argument(...),) -> None:
     """Set api-gateway configuration"""
+    set_service("apigateway", backend)
+
+
+def set_service(service: str, backend: str) -> None:
+    """Set configuration"""
     path = Path(os.getcwd()) / ".minos-project.yaml"
 
     if not path.exists():
@@ -138,20 +57,16 @@ def apigateway(
         if "services" in data and not data["services"]:
             data["services"] = dict()
 
-    if "services" in data and "api-gateway" in data["services"]:
-        console.print("Database already set")
+    if "services" in data and service in data["services"]:
+        console.print(f"{service} already set")
         raise typer.Exit()
     else:
-        console.print(":wrench: Setting api-gateway config\n")
-        fetcher = TemplateFetcher.from_name(f"project-apigateway-{backend}-init", VERSION)
-        processor = TemplateProcessor.from_fetcher(
-            fetcher,
-            Path.cwd(),
-            defaults={"project_name": Path.cwd().name}
-        )
+        console.print(f":wrench: Setting {service} config\n")
+        fetcher = TemplateFetcher.from_name(f"project-{service}-{backend}-init", VERSION)
+        processor = TemplateProcessor.from_fetcher(fetcher, Path.cwd(), defaults={"project_name": Path.cwd().name})
         processor.render()
 
-        data["broker"] = backend
+        data[service] = backend
         yaml.dump(data, sort_keys=False)
 
 
