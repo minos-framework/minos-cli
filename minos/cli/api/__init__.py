1865c371dc24b41f49f0798cbac40bbef9d6fa58
albamig <albamigh@gmail.com>
2022-02-02 11:04:23 +0100

    - Refactored cmd structure. - Refactored tests.
    issue #45

--- minos/cli/api/__init__.py
+++ minos/cli/api/__init__.py
@@ -3,14 +3,14 @@ import typer
 from ..consoles import (
     console,
 )
-from .microservice import app as microservice_app
-from .project import app as project_app
+from .init import app as init_app
+from .new import app as new_app
 from .set import app as set_app
 from .utils import app as utils_app
 
 app = typer.Typer(add_completion=False)
-app.add_typer(microservice_app, name="microservice")
-app.add_typer(project_app, name="project")
+app.add_typer(init_app, name="init")
+app.add_typer(new_app, name="new")
 app.add_typer(utils_app, name="utils")
 app.add_typer(set_app, name="set")
 
