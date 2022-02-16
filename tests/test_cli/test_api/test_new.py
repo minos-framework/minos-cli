eb17d06ed0aada7aa352fcd1a4ddf8aa06ac8e5b
albamig <albamigh@gmail.com>
2022-02-16 13:18:28 +0100

    - Project file created dynamically. - Test for no project file.
    issue #73

--- tests/test_cli/test_api/test_new.py
+++ tests/test_cli/test_api/test_new.py
@@ -21,6 +21,14 @@ from minos.cli import (
 
 
 class TestNew(unittest.TestCase):
+    def setUp(self) -> None:
+        self.minos_project_file = Path.cwd() / ".minos-project.yaml"
+        self.minos_project_file.touch()
+
+    def tearDown(self) -> None:
+        if self.minos_project_file.exists():
+            self.minos_project_file.unlink()
+
     def test_main(self):
         self.assertEqual(__main__.main, main)
 
@@ -43,6 +51,18 @@ class TestNew(unittest.TestCase):
                 self.assertEqual(0, result.exit_code)
 
                 self.assertEqual(1, mock.call_count)
+
+    def test_new_microservice_no_project_file(self) -> None:
+        self.minos_project_file.unlink()
+
+        with TemporaryDirectory() as tmp_dir_name:
+            path = Path(tmp_dir_name) / "product"
+            with patch("minos.cli.TemplateProcessor.render") as mock:
+                result = CliRunner().invoke(app, ["new", "microservice", str(path)])
+
+                self.assertEqual(1, result.exit_code)
+
+                self.assertEqual(0, mock.call_count)
 
 
 if __name__ == "__main__":
