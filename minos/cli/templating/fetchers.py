75555ded2e7c78fe7bd621bc8f03775fec477982
albamig <albamigh@gmail.com>
2022-04-01 14:10:57 +0200

    - Updated templates version.
    issue #?

--- minos/cli/templating/fetchers.py
+++ minos/cli/templating/fetchers.py
@@ -21,7 +21,7 @@ from ..consoles import (
 )
 
 TEMPLATE_URL: Final[str] = "https://github.com/minos-framework/minos-templates/releases/download"
-TEMPLATE_VERSION: Final[str] = "v0.2.0.dev0"
+TEMPLATE_VERSION: Final[str] = "v0.2.0"
 
 
 class TemplateFetcher:
